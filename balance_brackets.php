ef44dbd44a2ad1a660760758720a08c9ec5a117d
AldoS <sinani_aldo@hotmail.com>
2018-06-19 23:36:08 +0300

    Update balance_brackets.php

--- balance_brackets.php
+++ balance_brackets.php
@@ -2,20 +2,12 @@
 
 $arr=array();
 
+//example expression for testing
 $expression = "{{}(";
+
 $arr = str_split(trim($expression));
+
 echo find_balance($arr)."<br>";
-
-
-/*$_fp = fopen("c:\\xampp\\htdocs\\hackerrank\\demo_input\\balance_bracket_input.txt", "r");
-while( $line = fgets( $_fp ) )
-{
-  //array_push($arr,$line);
-  $arr = str_split(trim($line));
-  //echo $line."<br>";
-  echo find_balance($arr)."<br>";
-  //break;
-}*/
 
 function find_balance($exp)
 {
@@ -36,13 +28,11 @@ function find_balance($exp)
       array_pop($tmp);
     }
   }
+  
   if(count($tmp)==0)
     return "Yes";
   else
     return "NO";
 }
 
-
-
-
- ?>
+?>
