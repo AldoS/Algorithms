b230a75514900a2ee964e94a69328c088f25b6ca
AldoS <sinani_aldo@hotmail.com>
2018-06-19 23:41:36 +0300

    Update equalize_array.php

--- equalize_array.php
+++ equalize_array.php
@@ -1,28 +1,23 @@
 <?php
 
+//demo array for testing
 $arr = array(3,3,2,1,3);
 equalizeArray($arr);
 
 function equalizeArray($arr) {
-    // Complete this function
-    /*
+    /* 
+    example:
     3 3 4 4 5 6 8 9 6 4
     -------------------
     3 3 4 4 4 5 6 6 8 9
     */
-
-    //$CntValues = array_count_values($arr);
-    //$maxVal = max($CntValues);
-    //echo count($arr) - $maxVal;
-    //time complexity O(n) , O(nlogn)
-    //space complexity O(n)
-
     $cnt=count($arr);
     $CntValues = array();
     sort($arr);
     $i=1;
     $myCnt=0;
     $tmpCnt=1;
+    
     while(($myCnt <= $cnt/2) && $i<$cnt)
     {
         if($arr[$i] != $arr[$i-1])
@@ -37,7 +32,7 @@ function equalizeArray($arr) {
 
         $i++;
     }
-    echo $myCnt;*/
+    echo $myCnt;
 }
 
  ?>
