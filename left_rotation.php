187ef504ce3f93dad616714add6e6f993143e910
AldoS <sinani_aldo@hotmail.com>
2018-06-19 23:47:20 +0300

    Update left_rotation.php

--- left_rotation.php
+++ left_rotation.php
@@ -1,5 +1,6 @@
 <?php
 
+//demo arrays for testing
 $a = array(1,2,3,4,5);
 $b = array();
 $n = count($a);
@@ -7,8 +8,6 @@ $k = 4;   //rotates
 
 for($i=0;$i<$n;$i++)
 {
-    //$tmp=$a[$i];
-    //$a[$i]=$a[$i+$k];
     $point = $n-$k+$i;
     if($point >= $n)
       $b[$point-$n] = $a[$i];
@@ -19,21 +18,13 @@ print_r($b);
 
 
 /*
-for($i = 0; $i < 2; $i++){
-    print_r($a);
-    array_unshift($a, array_pop($a));
-    print_r($a);
-}
-*/
-
-/*
 function leftRotation($a,$d)
 {
 	//sample input --> 12345 ,4 step
 	//output --> 51234
 	array_push($a, array_shift($a));	//timeout :(
 
-	//enellaktikos tropos
+	//alternative way
 	$tmp = $a[$i];
 	unset($a[$i]);
 	array_push($a,$tmp);
