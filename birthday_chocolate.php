aaefe11a1ec5904a9f00d157b3064e055355bc83
AldoS <sinani_aldo@hotmail.com>
2018-06-19 23:36:54 +0300

    Update birthday_chocolate.php

--- birthday_chocolate.php
+++ birthday_chocolate.php
@@ -1,20 +1,19 @@
 <?php
 
+//random input for testing
 $n = 1;
 $s = array(4);
 $d = 4;
 $m = 1;
 solve($n, $s, $d, $m);
 
-function solve($n, $s, $d, $m){
-    // Complete this function
+function solve($n, $s, $d, $m) {
     $cnt = 0;
     $sum = array();
     $sum[0] = 0;
     for($i=0;$i<$n;$i++)
         $sum[$i+1] = $sum[$i] + $s[$i];
 
-    print_r($sum);
     for($i=0;$i<=$n-$m;$i++) {
       $tmp1 = $sum[$i+$m];
       $tmp2 = $sum[$i];
